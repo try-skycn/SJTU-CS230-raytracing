69edc8725388a9de894526e65df3faff03174880
Tianyao Chen <try.skycn@Gmail.com>
2017-03-26 19:38:45 +0800

    modify vec.hpp, fix some compile errors

--- src/vec.hpp
+++ src/vec.hpp
@@ -8,7 +8,7 @@ constexpr float EPS = 1e-4;
 struct Vec {
 	float x, y, z;
 	Vec(): x(0.), y(0.), z(0.) {}
-	Vec(float _x): x(_x), y(_y), z(_z) {}
+	Vec(float _x): x(_x), y(_x), z(_x) {}
 	Vec(float _x, float _y, float _z): x(_x), y(_y), z(_z) {}
 	// assignment operations
 	Vec &operator+=(const Vec &r) { x += r.x; y += r.y; z += r.z; return *this; }
@@ -25,12 +25,12 @@ struct Vec {
 	Vec operator*(float r) const { return Vec(x * r, y * r, z * r); }
 	Vec operator/(float r) const { return Vec(x / r, y / r, z / r); }
 	Vec operator-() const { return Vec(-x, -y, -z); }
-	friend Vec operator*(float l, const Vec &r) { return Vec(l   * r.x, l   * r.y, ;   * r.z); }
+	friend Vec operator*(float l, const Vec &r) { return Vec(l * r.x, l * r.y, l * r.z); }
 	// functional operations
 	float dot(const Vec &r) const { return x * r.x + y * r.y + z * r.z; }
 	Vec corss(const Vec &r) const { return Vec(y * r.z - z * r.y, z * r.x - x * r.z, x * r.y - y * r.x); }
 	float sqrlen() const { return dot(*this); }
-	float len() const { return sqrtf(sqrlen); }
+	float len() const { return std::sqrtf(sqrlen()); }
 	Vec unit() const { return *this / len(); }
 	Vec min(const Vec &r) const { return Vec(std::min(x, r.x), std::min(y, r.y), std::min(z, r.z)); }
 	Vec min(float r) const { return Vec(std::min(x, r), std::min(y, r), std::min(z, r)); }
