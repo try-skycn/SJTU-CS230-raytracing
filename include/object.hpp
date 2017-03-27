2be40192c58c0bc21b773ce7876d94350bdcad7d
Tianyao Chen <try.skycn@Gmail.com>
2017-03-27 15:58:21 +0800

    base class: object

--- include/object.hpp
+++ include/object.hpp
@@ -1,3 +1,23 @@
+#pragma once 
+
+#include "material.hpp"
+#include "ray.hpp"
+
+struct IntersectionResult {
+
+};
+
 struct Object {
-	
+	// members
+
+	Material material;
+
+	// constructors
+
+	Object(const Material &_material): material(_material) {
+	}
+
+	virtual IntersectionResult intersect(const Ray &ray) const = 0;
+
+	virtual Vec get_normal(const Vec &point) const = 0;
 };
