e3d8a123de4a3382966c759a8f7af5ad4016cb56
Tianyao Chen <try.skycn@Gmail.com>
2017-03-27 15:26:07 +0800

    save

--- src/scene.hpp
+++ src/scene.hpp
@@ -1,4 +1,14 @@
 #pragma once
 
+#include <vector>
 #include "vec.hpp"
 
+struct Object {
+	static Vec get_reflected(const Vec &in_dir, const Vec &normal) { return (in_dir - 2.0 * in_dir.dot(normal) * normal).unit(); }
+	static Vec get_refracted(const Vec &in_dir, const Vec &normal, float in_index, float out_index) { assert(false); return Vec(); }
+};
+
+struct Scene {
+	vector<Object> objects;
+
+};
